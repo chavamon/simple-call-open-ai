94982686686ced4a333e8e4712f3a19ccdd75b36
Salvador Valeriano lucas <salvador.valeriano@gmail.com>
2025-02-17 23:20:45 -0600

    model changed from gpt-4o-mini to ollama deepseek

--- oop/main.py
+++ oop/main.py
@@ -1,35 +1,28 @@
-import os
-from dotenv import load_dotenv
 import logging
-from openai import OpenAI
+import ollama
 
 # Logging configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.info("Logging is now configured!")
 
-# Load environment variables in a file called .env
-load_dotenv(override=True)
-api_key = os.getenv('OPENAI_API_KEY')
+# Constants
 
-# Check the key
+OLLAMA_API = "http://localhost:11434/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+MODEL = "deepseek-r1:8b"
 
-if not api_key:
-    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
-elif not api_key.startswith("sk-proj-"):
-    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
-elif api_key.strip() != api_key:
-    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
-else:
-    print("API key found and looks good so far!")
-    logger.info("API key found")
-
-openai = OpenAI()
 
 messages = [
-    {"role": "system", "content": "You are an assistant of Software Development, please respond in Spanish"},
-    {"role": "user", "content": "who is the most important developer in the world?"}
+    {"role": "system", "content": "You are an Sportive analyst, specialized in boxing."},
+    {"role": "user", "content": "Tell me the five best Mexican boxers ever, please your response in Spanish."},
 ]
 
-response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
-print(response.choices[0].message.content)
+payload = {
+        "model": MODEL,
+        "messages": messages,
+        "stream": False
+    }
+
+response = ollama.chat(model=MODEL, messages=messages)
+print(response['message']['content'])
